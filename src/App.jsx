cee7a18199606a479ecfa93228a9cab574dc66eb
Manishsuthar009 <sutharmanish8677@gmail.com>
2024-10-04 19:41:10 +0530

    created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const Hero = () => {
+    return (
+      <div className='App'>
+        <header className="hero-section">
+          <Navbar/>
+            <div className="hero-content">
+              <h1>Real Agency<br/>With Its Super<br/><span>Digital</span></h1>
+              <p>Let's start discussing your project</p>
+            </div>
+            <div className='whyus'>
+              {/* <h1>WhyUs?</h1> */}
+              <img src="src/Untitled design.png" alt="" />
+            </div>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      
+    );
+};
 
-export default App
+export default Hero;
